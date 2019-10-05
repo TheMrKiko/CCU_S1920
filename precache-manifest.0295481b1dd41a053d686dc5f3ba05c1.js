c9fa790588142380e2d8d9b77689d11b428b24df
Francisco Sousa <f.sou@hotmail.com>
2019-10-05 15:49:56 +0100

    Updates

---
+++ precache-manifest.0295481b1dd41a053d686dc5f3ba05c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28071443eb13930b12e351c774e64f7",
+    "url": "/CCU_S1920/index.html"
+  },
+  {
+    "revision": "9b0ea274e0790c2c974a",
+    "url": "/CCU_S1920/static/css/main.fc4ea178.chunk.css"
+  },
+  {
+    "revision": "6a0edfc929575d214b68",
+    "url": "/CCU_S1920/static/js/2.9e81fd0c.chunk.js"
+  },
+  {
+    "revision": "9b0ea274e0790c2c974a",
+    "url": "/CCU_S1920/static/js/main.a5f7eee8.chunk.js"
+  },
+  {
+    "revision": "83716980be9ed9494409",
+    "url": "/CCU_S1920/static/js/runtime-main.f21a358d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CCU_S1920/static/media/logo.25bf045c.svg"
+  }
+]);
