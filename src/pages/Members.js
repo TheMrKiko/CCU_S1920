27a8ce26f261e31a61ee1dae9d690f1c578aa9ad
Francisco Sousa <f.sou@hotmail.com>
2019-10-06 21:54:08 +0100

    :speech_balloon: Add members' roles

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -4,11 +4,11 @@ import { Page } from '../components';
 const Members = () => (
 	<Page>
 		<ul>
-			<li>83441 - Daniel Martins e Santos Ramalho</li>
-			<li>86505 - Ricardo Caetano Velhinho</li>
-			<li>86416 - Francisco Do Canto Sousa</li>
-			<li>86372 - Alexandre Miguel Vicente Galambas</li>
-			<li>86430 - Guilherme Nuno Vicente Galambas</li>
+			<li>83441 - Daniel Martins e Santos Ramalho (User Research)</li>
+			<li>86505 - Ricardo Caetano Velhinho (Project Management)</li>
+			<li>86416 - Francisco Do Canto Sousa (Frontend)</li>
+			<li>86372 - Alexandre Miguel Vicente Galambas (Design)</li>
+			<li>86430 - Guilherme Nuno Vicente Galambas (Backend)</li>
 		</ul>
 	</Page>
 )
