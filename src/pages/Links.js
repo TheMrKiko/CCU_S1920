5aa5cda768b4690a95d6e1b759b1234c2c9db524
Francisco Sousa <f.sou@hotmail.com>
2019-10-31 12:36:41 +0000

    :lipstick: Links are a list

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -3,15 +3,23 @@ import { Page } from '../components';
 
 const Links = () => (
 	<Page>
-		<a href={`${process.env.PUBLIC_URL}/pdf/project_pitch.pdf`}>
-			Project Pitch and stakeholders
-		</a>
-		<a href={`${process.env.PUBLIC_URL}/pdf/user_research.pdf`}>
-			User Research
-		</a>
-		<a href={`${process.env.PUBLIC_URL}/pdf/prioritize_development.pdf`}>
-			Prioritize Development
-		</a>
+		<ul>
+			<li>
+				<a href={`${process.env.PUBLIC_URL}/pdf/project_pitch.pdf`}>
+					Project Pitch and stakeholders
+				</a>
+			</li>
+			<li>
+				<a href={`${process.env.PUBLIC_URL}/pdf/user_research.pdf`}>
+					User Research
+				</a>
+			</li>
+			<li>
+				<a href={`${process.env.PUBLIC_URL}/pdf/prioritize_development.pdf`}>
+					Prioritize Development
+				</a>
+			</li>
+		</ul>
 	</Page>
 )
 
