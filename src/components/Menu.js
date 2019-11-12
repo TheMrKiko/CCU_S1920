1019a949cdea9c82149fc6a1afe48b8bb1bcc1c7
Francisco Sousa <f.sou@hotmail.com>
2019-11-12 15:40:23 +0000

    :sparkles: Add Menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = () => (
+	<ul id="menu">
+		<li className="menu-link" data-menuanchor="home">
+			<a href="#home">Home</a>
+		</li>
+		<li className="menu-link" data-menuanchor="work">
+			<a href="#work">Work</a>
+		</li>
+		<li className="menu-link" data-menuanchor="aboutus">
+			<a href="#aboutus">About Us</a>
+		</li>
+	</ul>
+)
+
+export default Menu
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Page from './Page'
 import Pages from './Pages'
 import Header from './Header'
+import Menu from './Menu'
 
-export { Page, Pages, Header }
+export { Page, Pages, Header, Menu }
