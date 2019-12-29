ed686fc8b63c21b3ca83a66240af5edac0d35be7
Francisco Sousa <f.sou@hotmail.com>
2019-12-29 20:28:43 +0000

    :lipstick: List of links is a block

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,48 +1,62 @@
 import React from 'react'
 import { Page } from '../components';
+import metro from '../img/metro.jpg'
+
+const FileLink = ({ children, href }) => (
+	<a href={href}>
+		<div className="file-link">
+			<div className="file-link-img">
+				<img alt="link" src={metro} />
+			</div>
+			<span className="file-link-text">
+				{children}
+			</span>
+		</div>
+	</a>
+)
 
 const Links = () => (
 	<Page>
-		<ul>
+		<ul id="file-list">
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/project_pitch.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/project_pitch.pdf`}>
 					Project Pitch and stakeholders
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/user_research.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/user_research.pdf`}>
 					User Research
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/prioritize_development.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/prioritize_development.pdf`}>
 					Prioritize Development
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/low_fidelity_prototype.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/low_fidelity_prototype.pdf`}>
 					Low-Fidelity Prototype
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/formative_user_study​.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/formative_user_study​.pdf`}>
 					Formative User Study
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/usability_testing.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/usability_testing.pdf`}>
 					Usability Testing
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/fully_functional_prototype.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/fully_functional_prototype.pdf`}>
 					Fully Functional Prototype
-				</a>
+				</FileLink>
 			</li>
 			<li>
-				<a href={`${process.env.PUBLIC_URL}/pdf/final_presentation.pdf`}>
+				<FileLink href={`${process.env.PUBLIC_URL}/pdf/final_presentation.pdf`}>
 					Final Presentation
-				</a>
+				</FileLink>
 			</li>
 		</ul>
 	</Page>
